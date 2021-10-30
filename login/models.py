4f5b04f111deaeeef1e3a93b3d88b7c698cc4ef3
Youngcius <youngconfucian@outlook.com>
2021-10-30 16:17:38 -0700

    update database

---
+++ login/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+
+
+class DDD(models.Model):
+    pass
--- quagent/settings.py
+++ quagent/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'login.apps.LoginConfig',
     # 'post.apps.PostConfig',
 
 ]
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'quagent.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,10 +77,10 @@ DATABASES = {
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    # },
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'local_bbs',
+        'NAME': 'quagent',
         'USER': 'root',
         'PASSWORD': 'mimawangle123',
         'HOST': '127.0.0.1',
